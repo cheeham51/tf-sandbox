39e97b4571c56284c65d872850872c8753dbfd45
Tony Du <tony@frankieone.com>
2022-08-21 23:10:06 +1000

    Fix codebuild

--- modules/code-pipeline/code-build.tf
+++ modules/code-pipeline/code-build.tf
@@ -41,7 +41,7 @@ POLICY
 resource "aws_codebuild_project" "example" {
   name          = "test-project"
   description   = "test_codebuild_project"
-  build_timeout = "29"
+  build_timeout = "5"
   service_role  = aws_iam_role.example.arn
 
   artifacts {
@@ -57,7 +57,7 @@ resource "aws_codebuild_project" "example" {
 
   source {
     type            = "CODEPIPELINE"
-    location        = "codebuild-buildspec.yml"
+    buildspec        = "codebuild-buildspec.yml"
   }
 
   tags = {
