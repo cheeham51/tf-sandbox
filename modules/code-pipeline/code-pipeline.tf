8cad082370ad9aedf6618410cf4d24c47b8c7c47
Tony Du <tony@frankieone.com>
2023-07-04 20:02:35 +1000

    enable pipeline

--- modules/code-pipeline/code-pipeline.tf
+++ modules/code-pipeline/code-pipeline.tf
@@ -67,10 +67,10 @@ resource "aws_s3_bucket" "codepipeline_bucket" {
   bucket = "test-tf-bucket-dtony"
 }
 
-resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
-  bucket = aws_s3_bucket.codepipeline_bucket.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
+#   bucket = aws_s3_bucket.codepipeline_bucket.id
+#   acl    = "private"
+# }
 
 resource "aws_iam_role" "codepipeline_role" {
   name = "test-role"
