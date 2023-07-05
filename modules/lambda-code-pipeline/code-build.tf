8e26e38b15d38b77907af68ad20e545d03550b0e
Tony Du <tony@frankieone.com>
2023-07-05 18:06:10 +1000

    add

--- modules/lambda-code-pipeline/code-build.tf
+++ modules/lambda-code-pipeline/code-build.tf
@@ -50,7 +50,7 @@ resource "aws_codebuild_project" "example" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image                       = "aws/codebuild/amazonlinux2-aarch64-standard:3.0	"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
   }
