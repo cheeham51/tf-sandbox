80bc2307a8635060befe9f77c2881b85654e5880
Tony Du <tony@frankieone.com>
2023-07-05 17:20:33 +1000

    Add

--- modules/lambda-code-pipeline/code-build.tf
+++ modules/lambda-code-pipeline/code-build.tf
@@ -39,7 +39,7 @@ POLICY
 }
 
 resource "aws_codebuild_project" "example" {
-  name          = "test-project"
+  name          = "test-project-1"
   description   = "test_codebuild_project"
   build_timeout = "5"
   service_role  = aws_iam_role.example.arn
