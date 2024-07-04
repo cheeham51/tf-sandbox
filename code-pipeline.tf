cc7ba3c3b4b74e021e10caee8622dc46b3636cf4
Tony Du <tony@frankieone.com>
2024-07-04 11:44:47 +1000

    Enable codepipeline

--- code-pipeline.tf
+++ code-pipeline.tf
@@ -1,6 +1,6 @@
-# module "test-code-pipeline" {
-#   source = "./modules/code-pipeline"
-# }
+module "test-code-pipeline" {
+  source = "./modules/code-pipeline"
+}
 
 # module "lambda-test-code-pipeline" {
 #   source = "./modules/lambda-code-pipeline"
